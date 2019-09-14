11bfaa061682300c60ee5bf9b793cdaa2a94b1c3
damiao-classroom <lgc928091232@163.com>
2019-09-14 21:13:31 +0800

    master

---
+++ index.vr.js
@@ -0,0 +1,17 @@
+import React,{ Component } from 'react';
+import {
+  AppRegistry,
+  View,
+} from 'react-vr';
+
+export default class react_360 extends Component {
+  render() {
+    return (
+      <View>
+        
+      </View>
+    );
+  }
+};
+
+AppRegistry.registerComponent('react_360', () => react_360);
